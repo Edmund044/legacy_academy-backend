1c41ccf6cf81cb3273bde8c7a7e48b220d3fc326
edmund001-eng <edmund.opiyo@cropnuts.com>
2026-09-25 17:04:34 +0300

    change remove user id

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -744,7 +744,7 @@ class StatementFilter(BaseModel):
 class GuardianCreate(BaseModel):
     first_name: str
     last_name: str
-    user_id: Optional[UUID] = None
+    #user_id: Optional[UUID] = None
     #player_id: Optional[UUID] = None
     relationship_type: str = None
     whatsapp_phone: Optional[str] = None
